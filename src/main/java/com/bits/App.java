5a0a08c06bb8d2cf5fb0000bcb92dc68d55a1898
Arshika Mahato <2022wa86225@wilp.bits-pilani.ac.in>
2026-09-25 11:55:41 +0530

    Update message to trigger CI build

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,7 +2,7 @@ package com.bits;
 
 public class App {
     public static String getMessage() {
-        return "Hello DevOps! Build 1 - Jenkins CI Lab by 2022wa86225";
+        return "Hello DevOps! Build 2 - Code change triggered Jenkins CI automatically";
     }
 
     public static void main(String[] args) {
